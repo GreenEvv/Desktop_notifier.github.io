0fb324446409e622c4627763415cdfb1b0745df3
mariajoseph230 <82135392+mariajoseph230@users.noreply.github.com>
2021-09-28 13:19:58 +0530

    Update minions.py

--- minions.py
+++ minions.py
@@ -3,10 +3,10 @@ from plyer import notification
 notification.notify(title="Heyy You!!! DRINK SOME WATER.. ",
                     message='Your Skin,your Hair,your Mind and your Body will Thank you',
                     app_icon="drink.ico",
-                    timeout=8)
+                    timeout=10)
 
 from plyer import notification
 notification.notify(title="STAY HOME ..STAY SAFE ..STAY HAPPY.. ",
                  message='Your Body will Thank you',
                  app_icon="mask.ico",
-                 timeout=15)
+                 timeout=10)
